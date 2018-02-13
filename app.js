e6cd7dfda482b1805e1ca09ed5179279de0a8f8c
alferguson <alferguson@berkeley.edu>
2018-02-13 10:08:18 -0800

    had issues with max call stack, fixed it and now function works

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 `use strict`
+// example of palindrome: racecar
+function palindrome(str) {
+    if (!str[1]) {
+        console.log("This is a palindrome");
+    }
+    else if (str[0] === str[str.length-1]) {
+        str = str.slice(1, str.length-1);
+        return palindrome(str);
+    }
+    else {
+        console.log("this is def not a palindrome");
+    }
+};
+
+palindrome("YYY");
+// palindrome("dingus");
+// palindrome("x");
